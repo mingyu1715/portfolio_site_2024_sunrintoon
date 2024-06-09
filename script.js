d1d4520fc50d9614fb609603b97161f81fad4032
mingyu1715 <snan46209@gmail.com>
2024-06-09 19:27:47 +0900

    portfolio_site_2024

---
+++ script.js
@@ -0,0 +1,39 @@
+let currentPage = 1;
+
+function toggleClass(e, toggleClassName) {
+  if(e.className.includes(toggleClassName)) {
+    e.className = e.className.replace(' ' + toggleClassName, '');
+  } else {
+    e.className += ' ' + toggleClassName;
+  }
+}
+
+function movePage(e, page) {
+  if (page == currentPage) {
+    currentPage+=2;
+    toggleClass(e, "left-side");
+    toggleClass(e.nextElementSibling, "left-side");
+    
+  }
+  else if (page = currentPage - 1) {
+    currentPage-=2;
+    toggleClass(e, "left-side");
+    toggleClass(e.previousElementSibling, "left-side");
+  }
+  
+}
+
+function movePage(e, page) {
+  if (page == currentPage) {
+    currentPage+=2;
+    toggleClass(e, "left-side");
+    toggleClass(e.nextElementSibling, "left-side");
+    document.getElementById("instruction").style.opacity = "0"; // instruction 부분 숨기기
+  }
+  else if (page == currentPage - 1) {
+    currentPage-=2;
+    toggleClass(e, "left-side");
+    toggleClass(e.previousElementSibling, "left-side");
+    document.getElementById("instruction").style.opacity = "1"; // instruction 부분 다시 보이게 하기
+  }
+}
